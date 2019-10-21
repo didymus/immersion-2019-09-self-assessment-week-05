06eab0ad1c2c4f64d121d536a6c326065aeb81aa
thomasmudge <alephcult@gmail.com>
2019-10-21 09:36:46 -0500

    Installed modules, connected to DB, added skeletons of `GET` and `POST` reqs

--- node-express-sequelize-crud/index.js
+++ node-express-sequelize-crud/index.js
@@ -12,3 +12,19 @@ User.sync({ force: true })
       console.log(`node-express-sequelize listening on ${PORT}`);
     });
   });
+
+User.get('/api/users', (req, res) => {
+// - [] Get all the users from the database __using the`User` model__
+// - [] Respond with a`200` status code and an array of the users in the body
+// - [] If the query to the database fails, you should log the error and send back a`500` status code
+});
+
+User.post('/api/users', (req, res) => {
+  // -[] Accept a JSON body that includes a`"username"` property:
+  // ```json
+  //   {
+  //     "username": "Alice"
+  //   }
+  //   ```
+})
+
