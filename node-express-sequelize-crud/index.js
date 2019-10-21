a54e77adf087518bc6b34eeffda573bb6b7a1a50
thomasmudge <alephcult@gmail.com>
2019-10-21 09:43:49 -0500

    Added skeletons for `PUT` and additional `GET` req

--- node-express-sequelize-crud/index.js
+++ node-express-sequelize-crud/index.js
@@ -29,5 +29,26 @@ User.post('/api/users', (req, res) => {
   // -[] Create a new user in the database __using the`User` model__
   // - [] Respond with a`201` status code and an object representing the user in the body
   // - [] If the query to the database fails, you should log the error and send back a`500` status code
-})
+});
 
+User.get('/api/users/:id', (req, res) => {
+  // - [] On a`GET /api/users/:id` you server should:
+  // - [] Get the user in the database with the`id` __using the`User` model__
+  // - [] Respond with a`200` status code and an object representing the user in the body
+  // - [] If a user with the`id` does not exist, you should send back a`404` status code
+  // - [] If the query to the database fails, you should log the error and send back a`500` status code
+
+});
+
+User.put('/api/users/:id', (req, res) => {
+  // -[] Accept a JSON body that includes a`"username"` property:
+  // ```json
+  //   {
+  //     "username": "Bob"
+  //   }
+  //   ```
+  // - [] Update the user in the database with the`id` __using the`User` model__
+  // - [] Respond with a`200` status code
+  // - [] If a user with the`id` does not exist, you should send back a`404` status code
+  // - [] If the query to the database fails, you should log the error and send back a`500` status code
+});
